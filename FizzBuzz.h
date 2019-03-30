d090a7d56b5a544b490dae0b94ce560644a78f37
sergtello <sergio92tello@gmail.com>
2019-03-30 11:20:40 -0500

    Remastered

--- FizzBuzz.h
+++ FizzBuzz.h
@@ -1,21 +1,36 @@
-#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
 
 int main(){
+vector<int> Fizz,Buzz,FizzBuzz;
 
 for (int i=1;i<=100;i++){
 	if(i%15==0) {
-		cout<<"FizzBuzz"<<endl;
+		//cout<<"FizzBuzz"<<endl;
+		FizzBuzz.push_back(i);
 		continue;
 	}
 	if(i%3==0){
-		cout<<"Fizz"<<endl;
+		//cout<<"Fizz"<<endl;
+		Fizz.push_back(i);
 	continue;}
 	
 	if(i%5==0){
-		cout<<"Buzz"<<endl;
+		//cout<<"Buzz"<<endl;
+		Fizz.push_back(i);
 	continue;}
 	cout<<i<<endl;
 }
+
+for (int i=0;i<Fizz.size();i++) cout<<Fizz[i]<<"\t";
+
+cout<<endl;
+
+for (int i=0;i<Buzz.size();i++) cout<<Buzz[i]<<"\t";
+
+cout<<endl;
+
+for (int i=0;i<FizzBuzz.size();i++) cout<<FizzBuzz[i]<<"\t";
+
 return 0;
 }
